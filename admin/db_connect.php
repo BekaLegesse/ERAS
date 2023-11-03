334a2dff0c405c20f51b5db15e3bb6baf7c9f3b1
BekaLegesse <61188181+BekaLegesse@users.noreply.github.com>
2023-11-03 12:47:48 +0300

    Update db_connect.php
    new

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,7 +1,16 @@
 <?php
-$serverName = "project-phase2.mysql.database.azure.com";
-$username = "projectphase2";
-$password = "Nedamcoacademy2";
-$databaseName = "eras";
-$conn = new mysqli($serverName, $username, $password, $databaseName) or die("Could not connect to mysql" . mysqli_error($con));
+$server = "project-phase2.mysql.database.azure.com";
+$username = "projectphase2"; 
+$password = "Nedamcoacademy2"; 
+$database = "eras";
+
+
+// Create a database connection
+$conn = new mysqli($server, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 ?>
