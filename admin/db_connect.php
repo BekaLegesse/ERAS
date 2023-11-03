c8755b40bb50f6f303b72c3b43070ec18fca44af
beka <bekalegesse9@gmail.com>
2023-11-03 08:05:23 +0300

    new commit

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -4,3 +4,4 @@ $username = "projectphase2";
 $password = "Nedamcoacademy2";
 $databaseName = "eras";
 $conn = new mysqli($serverName, $username, $password, $databaseName) or die("Could not connect to mysql" . mysqli_error($con));
+?>
